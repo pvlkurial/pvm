1dd45d7e6e0b2551d385d7e3bb09bdb04adb0ddc
Pavel Kurial <kuriapav@fit.cvut.cz>
2026-03-16 21:24:27 +0100

    Add laser icon

--- pvm-frontend/src/app/_components/Footer.tsx
+++ pvm-frontend/src/app/_components/Footer.tsx
@@ -1,9 +1,18 @@
+import Image from "next/image";
+
 export default function Footer() {
   return (
     <footer className="bg-black-900 border-t border-neutral-800 mt-auto py-6 p-5">
       <div className="max-w-7xl mx-auto px-4 text-center">
-        <p className="text-neutral-400 text-sm">
+        <p className="text-neutral-400 text-sm flex items-center justify-center gap-2">
           {new Date().getFullYear()} PVM - Made By Laser
+          <Image
+            src={`/misc/laser.svg`}
+            alt={"Laser"}
+            width={68}
+            height={68}
+            className="mp-style-svg"
+          />
         </p>
       </div>
     </footer>
